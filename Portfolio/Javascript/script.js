5b4d639cda7d99a784a6dfdcfd0a7902fb9e8eeb
Aakriti <aakriti.1012@gmail.com>
2020-01-13 15:32:07 +0530

    Adding Projects
    1. Portfolio
    2.  Survey Form

---
+++ Portfolio/Javascript/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+  $(window).resize(function(){location.reload();});
+
+  $('.mobile-quote').hide();
+
+  if ( $( window ).width() > 708 ){
+    $('a').click(function() {
+      $('a').removeClass('selected');
+      $(this).addClass('selected');
+      current = $(this);
+      $('.wrapper').scrollTo($(this).attr('href'), 800);
+      return false;
+    });
+
+    $('.quote').typeIt({
+     strings: ["Hi, I am Aakriti.", "I believe a great design can make a good idea even better.", "I create websites that have both:", "BEAUTY and BRAIN."],
+     speed: 80,
+     autoStart: true
+  });
+
+    $(".move-ahead").delay(20000).show("fade", 2000);
+  }
+
+  $('.nav-menu-bars').click(function(){
+    $( ".nav-menu" ).toggle("fade", "slow");
+    //$(this).find('i').toggleClass('fa-bars fa-arrow-right')
+  });
+
+  
+  if ( $( window ).width() < 708 ){
+    $(".nav-menu-bars").click(function() { 
+      return (this.tog = !this.tog) ? menu() : menuClose();
+    });
+
+    $('.nav-menu ul li a').click(function(){
+     $('.nav-menu').hide();
+     $('body').animate({ left: "0px"});
+   });
+
+   $('.mobile-quote').show();
+   $('.mobile-quote').typeIt({
+     strings: ["Hi, I am Aakriti. I believe a great design can make a good idea even better.", "I create websites that have both:", "BEAUTY and BRAIN."],
+     speed: 80,
+     autoStart: true
+  });
+  $(".move-ahead").delay(10000).show("fade", 2000);
+}
+  
+});
+
+function menu() {
+    $('body').animate({ left: "130px"}, 200);
+}
+
+function menuClose() {
+  $( ".nav-menu" ).hide();
+  $('body').animate({ left: "0px"}, 200);
+}
---
+++ Survey Form/README.md
@@ -0,0 +1,5 @@
+# FCC-Survey-Form
+This is repository for a freeCodeCamp project building a survey form. <br>
+For more information: https://www.freecodecamp.org/learn/responsive-web-design/responsive-web-design-projects/build-a-survey-form
+<br>
+Preview: https://aakriti94.github.io/FCC-Survey-Form/
